c1394fa44b2c5eda122cea3ac9e5a958f64021c8
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-04-26 17:44:55 +0300

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,12 @@
 import { deleteContact } from 'redux/contactsSlice';
-import React from 'react';
+import { selectContacts } from 'redux/selectors';
 import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import s from '../ContactList/ContactList.module.scss';
-import { selectContacts } from 'redux/selectors';
 
 function ContactList() {
   const distpatch = useDispatch();
