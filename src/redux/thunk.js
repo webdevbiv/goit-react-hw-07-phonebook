4d7b9a0da875a0e316b24ac82b03fa97531cc66e
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-05-02 15:17:31 +0300

    final-fix

--- src/redux/thunk.js
+++ src/redux/thunk.js
@@ -5,35 +5,41 @@ import {
   deleteContact,
 } from '../services/contactsAPI';
 
-export const getContactsThunk = createAsyncThunk('contacts/get', async () => {
-  try {
-    const contacts = await getContacts();
-    return contacts;
-  } catch (error) {
-    console.error(error);
-    return error.message;
+export const getContactsThunk = createAsyncThunk(
+  'contacts/get',
+  async (_, thunkAPI) => {
+    try {
+      const contacts = await getContacts();
+      return contacts;
+    } catch (error) {
+      console.error(error);
+      return thunkAPI.rejectedValue(error.message);
+    }
   }
-});
+);
 
-export const addContactThunk = createAsyncThunk('contacts/add', async data => {
-  try {
-    const res = await addContact(data);
-    return res;
-  } catch (error) {
-    console.error(error);
-    return error.message;
+export const addContactThunk = createAsyncThunk(
+  'contacts/add',
+  async (data, thunkAPI) => {
+    try {
+      const res = await addContact(data);
+      return res;
+    } catch (error) {
+      console.error(error);
+      return thunkAPI.rejectedValue(error.message);
+    }
   }
-});
+);
 
 export const deleteContactThunk = createAsyncThunk(
   'contacts/delete',
-  async id => {
+  async (id, thunkAPI) => {
     try {
       const deletedId = await deleteContact(id);
       return deletedId;
     } catch (error) {
       console.error(error);
-      return error.message;
+      return thunkAPI.rejectedValue(error.message);
     }
   }
 );
